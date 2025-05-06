d5c001e875d76b4bfecb4f88ab06faa373b8cda9
durbanoutie <durbanout13@gmail.com>
2025-05-06 08:19:39 +0200

    Jumping Back and Forth

--- vm.c
+++ vm.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -83,6 +84,7 @@ static void concatenate(){
 
 static InterpretResult run(){
 #define READ_BYTE() (*vm.ip++)
+#define READ_SHORT() (vm.ip+=2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 
@@ -203,6 +205,23 @@ static InterpretResult run(){
                 printf("\n");
                 break;
             }
+            case OP_JUMP:{
+                uint16_t offset = READ_SHORT();
+                vm.ip += offset;
+                break;
+            }
+            case OP_JUMP_IF_FALSE:{
+                uint16_t offset = READ_SHORT();
+                if(isFalsey(peek(0))){
+                    vm.ip += offset;
+                }
+                break;
+            }
+            case OP_LOOP:{
+                uint16_t offset = READ_SHORT();
+                vm.ip -= offset;
+                break;
+            }
             case OP_RETURN:{
                 return INTERPRET_OK;
             }
@@ -211,6 +230,7 @@ static InterpretResult run(){
         }
     }
 #undef READ_BYTE
+#undef READ_SHORT
 #undef READ_CONSTANT
 #undef READ_STRING
 #undef BINARY_OP
