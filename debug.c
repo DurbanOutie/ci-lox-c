c123f601da1299de68e0bd03ee8739f59ee5059f
durbanoutie <durbanout13@gmail.com>
2025-05-21 13:30:17 +0200

    Methods and Initializers

--- debug.c
+++ debug.c
@@ -32,6 +32,17 @@ static int constantInstruction(const char *name, Chunk *chunk, int offset){
     return offset + 2;
 }
 
+static int invokeInstruction(const char *name, Chunk *chunk, int offset){
+    uint8_t constant = chunk->code[offset + 1];
+    uint8_t argCount = chunk->code[offset + 2];
+
+    printf("%-16s (%d args) %4d '", name, argCount, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 3;
+
+}
+
 int disassembleInstruction(Chunk *chunk, int offset){
     printf("%04d ", offset);
 
@@ -43,6 +54,7 @@ int disassembleInstruction(Chunk *chunk, int offset){
 
     uint8_t instruction = chunk->code[offset];
     switch(instruction){
+        case OP_INVOKE:          return invokeInstruction("OP_INVOKE",          chunk, offset);
         case OP_CALL:            return byteInstruction("OP_CALL",              chunk, offset);
         case OP_GET_LOCAL:       return byteInstruction("OP_GET_LOCAL",         chunk, offset);
         case OP_SET_LOCAL:       return byteInstruction("OP_SET_LOCAL",         chunk, offset);
@@ -51,6 +63,7 @@ int disassembleInstruction(Chunk *chunk, int offset){
         case OP_GET_PROPERTY:    return constantInstruction("OP_GET_PROPERTY",  chunk, offset);
         case OP_SET_PROPERTY:    return constantInstruction("OP_SET_PROPERTY",  chunk, offset);
         case OP_CLASS:           return constantInstruction("OP_CLASS",         chunk, offset);
+        case OP_METHOD:          return constantInstruction("OP_METHOD",        chunk, offset);
         case OP_CONSTANT:        return constantInstruction("OP_CONSTANT",      chunk, offset);
         case OP_GET_GLOBAL:      return constantInstruction("OP_GET_GLOBAL",    chunk, offset);
         case OP_DEFINE_GLOBAL:   return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
