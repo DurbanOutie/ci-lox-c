a1a8fd718ffc04da8763ccc655d690fafc3ac689
durbanoutie <durbanout13@gmail.com>
2025-05-27 17:09:13 +0200

    Optimization

--- table.c
+++ table.c
@@ -21,7 +21,7 @@ void freeTable(Table *table){
 }
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key){
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1);
     Entry *tombstone = NULL;
     for(;;){
         Entry *entry = &entries[index];
@@ -36,7 +36,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key){
         }else if(entry->key == key){
             return entry;
         }
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -125,7 +125,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
         return NULL;
     }
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
 
     for(;;){
         Entry *entry = &table->entries[index];
@@ -136,7 +136,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
         }else if(entry->key->length == length && entry->key->hash == hash && memcmp(entry->key->chars, chars, length) == 0){
             return entry->key;
         }
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 
 
